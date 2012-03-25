ddb91e0813ee268b6cf7cfc9e28410a31a125288
ange_zhao <ange_zhao@163.com>
2012-03-25 21:46:10 +0800

    class

---
+++ huatuo360/Constants.h
@@ -0,0 +1,41 @@
+//
+//  Constants.h
+//  huatuo360
+//
+//  Created by Zhao Ange on 12-3-22.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+
+#define perpage 10
+NSString* const _baseUrl = @"http://www.huatuo360.com/m/";
+
+//login.php?userId=username&password=passwd
+NSString* const _login = @"login.php?";
+
+//regist.php?userId=username&password=passwd&email=email@e.com
+NSString* const _regist = @"regist.php?";
+
+//comment.php?hospid=医院id&impression=1(0推荐与否)&evaluation=评价&userId=用户id
+NSString* const _comment = @"comment.php?";
+
+//科室列表接口(分医院部分地区)getDepartmentList.php?hospid=医院id
+NSString* const _departmentList = @"getDepartmentList.php?";
+
+//评论列表getCommentList.php?hospid=医院id&doctorid=医生id&page=页数&perpage=分页数目
+NSString* const _commentList = @"getCommentList.php?";
+
+//医生介绍getDoctor.php?id=医生id
+NSString* const _doctor = @"getDoctor.php?";
+
+//医院介绍getHospital.php?id=220
+NSString* const _hospital = @"getHospital.php?";
+
+//医院排行getHospitalList.php?deptid=疾病/科室id&hospital=搜索的医院名&city=城市id&page=页数&perpage=分页数目
+NSString* const _hospitalList = @"getHospitalList.php?";
+
+//医生列表getDoctorList.php?deptid=疾病/科室id&hospid=医院id&doctor=医生名字&city=城市id&page=页数&perpage=分页数目
+NSString* const _doctorList = @"getDoctorList.php?";
+
+//疾病排行getDiseaseList.php?disease=搜索的疾病名&page=第几页&perpage=分页数目
+NSString* const _diseaseList = @"getDiseaseList.php?";
