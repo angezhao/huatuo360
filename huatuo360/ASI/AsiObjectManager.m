10f817d8d863e25b95fa8788ada460a0d762a56b
ange_zhao <ange_zhao@163.com>
2012-04-06 21:57:30 +0800

    test

--- huatuo360/ASI/AsiObjectManager.m
+++ huatuo360/ASI/AsiObjectManager.m
@@ -2,6 +2,12 @@
 #import "SBJson.h"
 #import "Constants.h"
 
+#import <sys/socket.h>
+#import <netinet/in.h>
+#import <arpa/inet.h>
+#import <netdb.h>
+#import <SystemConfiguration/SCNetworkReachability.h>
+
 @implementation AsiObjectManager
 @synthesize delegate;
 
@@ -101,5 +107,71 @@
     NSLog(@"%@", error);
     [delegate requestFailed:error];
 }
+
+
+//Snip, you know we're in the implementation...
+- (BOOL) connectedToNetwork
+{
+    // Create zero addy
+    struct sockaddr_in zeroAddress;
+    bzero(&zeroAddress, sizeof(zeroAddress));
+    zeroAddress.sin_len = sizeof(zeroAddress);
+    zeroAddress.sin_family = AF_INET;
+    
+    // Recover reachability flags
+    SCNetworkReachabilityRef defaultRouteReachability = SCNetworkReachabilityCreateWithAddress(NULL, (struct sockaddr *)&zeroAddress);
+    SCNetworkReachabilityFlags flags;
+    
+    BOOL didRetrieveFlags = SCNetworkReachabilityGetFlags(defaultRouteReachability, &flags);
+    CFRelease(defaultRouteReachability);
+    
+    if (!didRetrieveFlags)
+    {
+        return NO;
+    }
+    
+    BOOL isReachable = flags & kSCNetworkFlagsReachable;
+    BOOL needsConnection = flags & kSCNetworkFlagsConnectionRequired;
+    return (isReachable && !needsConnection) ? YES : NO;
+}
+
+//call like:
+-(void) start {
+    if (![self connectedToNetwork]) {
+        NSLog(@"conect fail");
+    } else {
+        //do something 
+    }
+}
+
+/*
++ (BOOL)isNetWork
+{
+    BOOL reachability;
+    Reachability *reach = [Reachability reachabilityWithHostName:@"192.168.3.1"];
+    switch ([reach currentReachabilityStatus]) {
+        case NotReachable:
+            //无网络连接
+            reachability = NO;
+            return reachability;
+            break;
+        case ReachableViaWWAN:
+            //使用3g网络
+            reachability = YES;
+            return reachability;
+            break;
+        case ReachableViaWiFi:
+            //使用wifi
+            reachability = YES;
+            return reachability;
+            break;
+            
+        default:
+            break;
+    }
+    
+    return reachability;
+}
+ */
 
 @end
